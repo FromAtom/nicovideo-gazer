c87e0cab5ccc7eef9f1af878a784b80f422d9364
Atom <fromatom@gmail.com>
2014-08-06 00:44:05 +0900

    Add a function toggle appear and disappear

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 (function (){
-function disapperClasses(class_names) {
+function toggleAppearOrDisappear(targetElement) {
+    if (targetElement.style.display == 'none') {
+        targetElement.style.display = null;
+    }
+    else {
+        targetElement.style.display = 'none';
+    }
+}
+
+function disappearClasses(class_names) {
     for (var i = 0; i < class_names.length; i++) {
-        var targetElem = document.getElementsByClassName(class_names[i]);
-        if (targetElem) {
-            targetElem[0].style.display = 'none';
+        var targetElement = document.getElementsByClassName(class_names[i])[0];
+        if (targetElement) {
+            toggleAppearOrDisappear(targetElement);
         }
     }
 }
 
-function disapperIds(id_names) {
+function disappearIds(id_names) {
     for (var i = 0; i < id_names.length; i++) {
-        var targetElem = document.getElementById(id_names[i]);
-        if (targetElem) {
-            targetElem.style.display = 'none';
+        var targetElement = document.getElementById(id_names[i]);
+        if (targetElement) {
+            toggleAppearOrDisappear(targetElement);
         }
     }
 }
+
 
 document.body.style.backgroundColor='red';
 
@@ -23,18 +33,18 @@ var id_names = [
     'siteHeader',
     'playlist',
     'videoExplorerExpand',
+    'playerTabWrapper',
     'bottomContentTabContainer',
     'footer'
 ];
-disapperIds(id_names);
+disappearIds(id_names);
 
 var class_names = [
     'videoHeaderOuter'
 ];
-disapperClasses(class_names);
+disappearClasses(class_names);
 
 document.getElementById('content').style.padding = '0px';
-
 
 document.body.style.backgroundColor='green';
 
