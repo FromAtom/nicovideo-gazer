91656546e669879fe47ae3fc61af4c6f6df17a35
Atom <fromatom@gmail.com>
2014-08-06 14:47:28 +0900

    marginとpaddingのトグルに対応

--- main.js
+++ main.js
@@ -8,8 +8,44 @@ function toggleAppearOrDisappear(targetElement) {
     }
 }
 
+
+function toggleSetBlankOrNot(target_name) {
+    var padding = '0px';
+    var margin  = '0px';
+
+    if ( localStorage.getItem(target_name) ) {
+        var blank_values = JSON.parse(localStorage.getItem(target_name));
+
+        padding = blank_values['padding'];
+        margin  = blank_values['margin'];
+
+        localStorage.removeItem(target_name);
+    }
+    else {
+        var original_padding = document.getElementById(target_name).style.padding;
+        var original_margin  = document.getElementById(target_name).style.margin;
+        var blank_values = {
+            padding: original_padding,
+            margin: original_margin
+        };
+        localStorage.setItem(target_name,  JSON.stringify(blank_values));
+    }
+
+    document.getElementById(target_name).style.padding = padding;
+    document.getElementById(target_name).style.margin  = margin;
+}
+
+function deleteBlank(id_names) {
+    for (var i = 0; i < id_names.length; i++) {
+        var targetElement = document.getElementById(id_names[i]);
+        if(targetElement) {
+            toggleSetBlankOrNot(id_names[i]);
+        }
+    }
+}
+
 function disappearClasses(class_names) {
-    for (var i = 0; i < class_names.length; i++) {
+    for (var i = 0; i < id_names.length; i++) {
         var targetElement = document.getElementsByClassName(class_names[i])[0];
         if (targetElement) {
             toggleAppearOrDisappear(targetElement);
@@ -26,7 +62,6 @@ function disappearIds(id_names) {
     }
 }
 
-
 document.body.style.backgroundColor='red';
 
 var id_names = [
@@ -34,6 +69,9 @@ var id_names = [
     'playlist',
     'videoExplorerExpand',
     'playerTabWrapper',
+    'textMarquee',
+    'chipWallList',
+    'wallImageContainer',
     'bottomContentTabContainer',
     'footer'
 ];
@@ -44,7 +82,12 @@ var class_names = [
 ];
 disappearClasses(class_names);
 
-document.getElementById('content').style.padding = '0px';
+var blank_names = [
+    'content',
+    'playerAlignmentArea',
+    'playerContainerWrapper'
+];
+deleteBlank(blank_names);
 
 document.body.style.backgroundColor='green';
 
